499f6c0d141e20955ce20dc235765c0cc700edb2
karenolea <karen.oleatoledo@gmail.com>
2021-08-20 22:03:13 -0400

    primer commit

---
+++ src/components/Inicio.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   
+
+    <h1 v-text="titulo"></h1>
+
+    <h2>Juegos de PC y consolas</h2>
+
+    
+   
+  </div>
+</template>
+<script>
+// import { mapState } from "vuex";
+
+export default {
+    computed: {
+    titulo() {
+      return "32bits";
+    },
+}
+};
+</script>
